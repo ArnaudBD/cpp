0c7768905853f0c30612fc0b6bb0de817a9e6964
ArnaudBD <arnaud.bijuduval@gmail.com>
2022-07-07 20:31:00 +0200

    wip: module 08

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,12 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+# include <algorithm>
+# include <vector>
+
+template <typename T>
+void    easyfind( T haystack, int needle )
+{
+    std::vector<int>    v(40, 2);
+}
+
+#endif
